e30f9797db80abd42044a410311407c89dae470e
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-02-17 18:30:08 -0300

    feat: create `Pipe` and `PipeGroup`

---
+++ Haskell/Models/Pipe.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DataKinds #-}
+
+module Models.Pipe where
+
+import Data.List (intercalate)
+import Models.Area
+import qualified Models.Area as Area
+
+pipeCharacter = '#'
+
+type UP = "UP"
+
+type DOWN = "DOWN"
+
+data Direction = UP | DOWN deriving (Show, Eq)
+
+data Pipe = Pipe
+  { originX :: Int,
+    originY :: Int,
+    width :: Int,
+    height :: Int,
+    direction :: Direction
+  }
+  deriving (Show)
+
+getArea :: Pipe -> Area
+getArea pipe =
+  Area.createFromString (originX pipe) (originY pipe) (toString pipe)
+
+toString :: Pipe -> String
+toString pipe
+  | direction pipe == DOWN = toDownString pipe
+  | otherwise = toUpString pipe
+
+toDownString :: Pipe -> String
+toDownString pipe = reverse (toUpString pipe)
+
+toUpString :: Pipe -> String
+toUpString pipe =
+  intercalate "\n" (toStringLines pipe)
+
+toStringLines :: Pipe -> [String]
+toStringLines pipe = replicate (height pipe) line
+  where
+    line = replicate (width pipe) pipeCharacter
