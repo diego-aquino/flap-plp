b0904eaa25ec22afa4744298400d83c26b64b8d1
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-03-24 09:27:17 -0300

    feat: create procedure to reset terminal cursor

--- Prolog/models/terminal.pl
+++ Prolog/models/terminal.pl
@@ -1,18 +1,21 @@
-:- module(terminal,[getTerminalWidth/1,getTerminalHeight/1,startPlayerInputThread/0,fetchFromThread/1]).
+:- module(terminal, [getTerminalWidth/1, getTerminalHeight/1, resetCursor/0, startPlayerInputThread/0, fetchFromThread/1]).
 
 getTerminalWidth(Width):- tty_size(_, Width).
 getTerminalHeight(Height):- tty_size(Height, _).
 
-startPlayerInputThread:- 
+resetCursor():-
+  tty_goto(0, 0).
+
+startPlayerInputThread:-
 thread_create(getPlayerInput, Id).
 
-getPlayerInput:- 
+getPlayerInput:-
   getkey(NewInput),
   handlePlayerInput(NewInput),
   getPlayerInput.
 
-getkey(X):-                                         
-  ttyflush, 
+getkey(X):-
+  ttyflush,
   get_single_char(X).
 
 handlePlayerInput(Input):- thread_send_message(main,Input).
