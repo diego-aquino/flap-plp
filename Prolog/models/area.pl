86a592d175c6e04ad42dbcf43cdd5bf48aa33172
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-03-23 21:17:48 -0300

    feat: implement area and line

--- Prolog/models/area.pl
+++ Prolog/models/area.pl
@@ -1,3 +1,35 @@
-:- module(area,[testMethodArea/1]).
+:- module(area, [createFromLines/2, createFromString/4]).
 
-testMethodArea(Param):- write(Param).
+:- use_module('../utils/strings').
+:- use_module(line).
+
+createFromLines(OccupiedLines, Area):-
+  Area = area(lines(OccupiedLines)).
+
+createFromString(OriginX, OriginY, String, Area):-
+  strings:lines(String, StringLines),
+  parseStringLines(OriginX, OriginY, StringLines, OccupiedLines),
+  createFromLines(OccupiedLines, Area).
+
+parseStringLines(OriginX, OriginY, StringLines, Lines):-
+  parseStringLinesRecursive(OriginX, OriginY, StringLines, 0, Lines).
+
+parseStringLinesRecursive(_OriginX, _OriginY, [], _Index, []).
+parseStringLinesRecursive(OriginX, OriginY, [StringLine | TailStringLines], Index, [Line | TailLines]):-
+  NextIndex is Index + 1,
+  parseStringLinesRecursive(OriginX, OriginY, TailStringLines, NextIndex, TailLines),
+  LineOriginY is OriginY + Index,
+  parseStringLine(OriginX, LineOriginY, StringLine, Line).
+
+parseStringLine(BaseX, BaseY, StringLine, Line):-
+  strings:size(StringLine, StringLineLength),
+  strings:removeLeadingSpaces(StringLine, StringLineWithoutLeadingSpaces),
+  strings:size(StringLineWithoutLeadingSpaces, NumberOfValidCharacters),
+  NumberOfLeadingSpaces is StringLineLength - NumberOfValidCharacters,
+  OriginX is BaseX + NumberOfLeadingSpaces,
+  OriginY = BaseY,
+  EndX is OriginX + NumberOfValidCharacters - 1,
+  line:create(OriginX, OriginY, EndX, Line).
+
+occupiedLines(Area, OccupiedLines):-
+  Area = area(lines(OccupiedLines)).
--- Prolog/models/line.pl
+++ Prolog/models/line.pl
@@ -1,3 +1,13 @@
-:- module(line,[testMethodLine/1]).
+:- module(line, [create/4]).
 
-testMethodLine(Param):- write(Param).
+create(OriginX, OriginY, EndX, Line):-
+  Line = line(originX(OriginX), originY(OriginY), endX(EndX)).
+
+originX(Line, OriginX):-
+  Line = line(originX(OriginX), originY(_), endX(_)).
+
+originY(Line, OriginY):-
+  Line = line(originX(_), originY(OriginY), endX(_)).
+
+endX(Line, EndX):-
+  Line = line(originX(_), originY(_), endX(EndX)).
