9afff3724524885e5ce853746aff4b0d811be06f
Murilo <murilo.castro@ccc.ufcg.edu.br>
2022-03-28 11:08:00 -0300

    feat: create bird

--- Prolog/models/bird.pl
+++ Prolog/models/bird.pl
@@ -1,3 +1,32 @@
-:- module(bird,[testMethodBird/1]).
+:- module(bird,[create/5]).
 
-testMethodBird(Param):- write(Param).
+:- use_module(area).
+
+create(OriginX, OriginY, VerticalSpeed, Bird) :-
+    Bird=bird(originX(OriginX), originY(OriginY), verticalSpeed(VerticalSpeed)).
+
+originX(Bird, OriginX) :-
+    Bird=bird(originX(OriginX), originY(_), verticalSpeed(_)).
+
+originY(Bird, OriginY) :-
+    Bird=bird(originX(_), originY(OriginY), verticalSpeed(_)).
+
+verticalSpeed(Bird, VerticalSpeed) :-
+    Bird=bird(originX(_), originY(_), verticalSpeed(VerticalSpeed)).
+
+tick(Bird, NewBird) :-
+  originX(Bird, OriginX),
+  originY(Bird, OriginY),
+  verticalSpeed(Bird, VerticalSpeed),
+  NewOriginY is OriginY + VerticalSpeed,
+  NewVerticalSpeed is VerticalSpeed + 0.2,
+  NewBird = bird(originX(OriginX), originY(NewOriginY), verticalSpeed(NewVerticalSpeed)).
+
+getArea(Bird, Area):-
+  toString(String),
+  originX(Bird, OriginX),
+  originY(Bird, OriginY),
+  area:createFromString(OriginX, OriginY, String, Area).
+
+toString(String):-
+  String = "== (.\n \\___\\".
