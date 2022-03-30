62d1efd1c8001e1dd39f401c34a6a8fb76a091ba
Murilo <murilo.castro@ccc.ufcg.edu.br>
2022-03-30 17:15:54 -0300

    refactor: pass old properties to new game state after increment score

--- Prolog/models/gameState.pl
+++ Prolog/models/gameState.pl
@@ -1,10 +1,11 @@
-:- module(gameState, [ create/6, incrementScore/3]).
+:- module(gameState, [ create/6, incrementScore/2]).
 
 playingScreenType('playing-screen').
 pausedScreenType('paused-screen').
 gameOverScreenType('game-over-screen').
 
 create(Bird, PipeGroups, Score, HighestScore, ScreenType, GameState) :-
+  isScreenType(ScreenType),
   GameState=gameState(bird(Bird), pipeGroups(PipeGroups), score(Score), highestScore(HighestScore), screenType(ScreenType)).
 
 bird(GameState, Bird) :-
@@ -16,18 +17,22 @@ pipeGroups(GameState, PipeGroups) :-
 score(GameState, Score) :-
   GameState=gameState(bird(_), pipeGroups(_), score(Score), highestScore(_), screenType(_)).
 
-incrementScore(GameState, Score, Increment) :-
-  NewScore is Score+Increment,
-  GameState=gameState(bird(_), pipeGroups(_), score(NewScore), highestScore(_), screenType(_)).
-
 highestScore(GameState, HighestScore) :-
   GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(HighestScore), screenType(_)).
 
 screenType(GameState, ScreenType) :-
-  isScreenType(ScreenType),
   GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(_), screenType(ScreenType)).
 
 isScreenType(ScreenType) :-
   playingScreenType(ScreenType);
   pausedScreenType(ScreenType);
   gameOverScreenType(ScreenType).
+
+incrementScore(GameState, Increment) :-
+  bird(GameState, Bird),
+  pipeGroups(GameState, PipeGroups),
+  score(GameState, Score),
+  highestScore(GameState, HighestScore),
+  screenType(GameState, ScreenType),
+  NewScore is Score+Increment,
+  GameState=gameState(bird(Bird), pipeGroups(PipeGroups), score(NewScore), highestScore(HighestScore), screenType(ScreenType)).
