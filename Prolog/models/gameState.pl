6112be8883292efa97e8d0a267f8b17f8cf54f8f
Murilo <murilo.castro@ccc.ufcg.edu.br>
2022-03-30 14:56:27 -0300

    fix: checks if is screen type

--- Prolog/models/gameState.pl
+++ Prolog/models/gameState.pl
@@ -1,35 +1,33 @@
-:- module(gameState,
-          [ create/6,
-            incrementScore/3
-          ]).
+:- module(gameState, [ create/6, incrementScore/3]).
 
 playingScreenType('playing-screen').
 pausedScreenType('paused-screen').
 gameOverScreenType('game-over-screen').
 
 create(Bird, PipeGroups, Score, HighestScore, ScreenType, GameState) :-
-    GameState=gameState(bird(Bird), pipeGroups(PipeGroups), score(Score), highestScore(HighestScore), screenType(ScreenType)).
+  GameState=gameState(bird(Bird), pipeGroups(PipeGroups), score(Score), highestScore(HighestScore), screenType(ScreenType)).
 
 bird(GameState, Bird) :-
-    GameState=gameState(bird(Bird), pipeGroups(_), score(_), highestScore(_), screenType(_)).
+  GameState=gameState(bird(Bird), pipeGroups(_), score(_), highestScore(_), screenType(_)).
 
 pipeGroups(GameState, PipeGroups) :-
-    GameState=gameState(bird(_), pipeGroups(PipeGroups), score(_), highestScore(_), screenType(_)).
+  GameState=gameState(bird(_), pipeGroups(PipeGroups), score(_), highestScore(_), screenType(_)).
 
 score(GameState, Score) :-
-    GameState=gameState(bird(_), pipeGroups(_), score(Score), highestScore(_), screenType(_)).
+  GameState=gameState(bird(_), pipeGroups(_), score(Score), highestScore(_), screenType(_)).
 
 incrementScore(GameState, Score, Increment) :-
-    NewScore is Score+Increment,
-    GameState=gameState(bird(_), pipeGroups(_), score(NewScore), highestScore(_), screenType(_)).
+  NewScore is Score+Increment,
+  GameState=gameState(bird(_), pipeGroups(_), score(NewScore), highestScore(_), screenType(_)).
 
 highestScore(GameState, HighestScore) :-
-    GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(HighestScore), screenType(_)).
+  GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(HighestScore), screenType(_)).
 
 screenType(GameState, ScreenType) :-
-    GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(_), screenType(ScreenType)).
+  isScreenType(ScreenType),
+  GameState=gameState(bird(_), pipeGroups(_), score(_), highestScore(_), screenType(ScreenType)).
 
-setScreenType(ScreenType) :-
-    playingScreenType(ScreenType),
-    pausedScreenType(ScreenType),
-    gameOverScreenType(ScreenType).
+isScreenType(ScreenType) :-
+  playingScreenType(ScreenType);
+  pausedScreenType(ScreenType);
+  gameOverScreenType(ScreenType).
